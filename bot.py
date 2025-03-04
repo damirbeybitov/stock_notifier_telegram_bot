755bd58ac46e564ef5813451863f2c778275bc2a
damirbeybitov <damirbey7@gmail.com>
2025-03-04 23:18:54 +0500

    Add message logging and chat ID retrieval to Telegram bot

--- bot.py
+++ bot.py
@@ -1,19 +1,32 @@
-import asyncio
-import logging
-from aiogram import Bot, Dispatcher, types
-from aiogram.types import Message
-from aiogram.filters import Command
+from telegram import Update
 from config import Config
 from logger import init_logger
+from telegram.ext import Application, CommandHandler
+from telegram.ext import MessageHandler, filters
 
 logger = init_logger("bot")
 
 BOT_TOKEN = Config.BOT_TOKEN
 app = Application.builder().token(BOT_TOKEN).build()
 
+def log_messages(update: Update):
+    message = update.message.text
+    chat_id = update.message.chat_id
+    logger.info(f"Received message: {message} from chat_id: {chat_id}")
+
+async def handle_message(update: Update, context):
+    log_messages(update)
+    # await update.message.reply_text("Сообщение получено!")
+
 async def start(update: Update, context):
+    log_messages(update)
     chat_id = update.message.chat_id
     await update.message.reply_text(f"Привет! Ты авторизован, {chat_id}!")
+
+async def get_chat_id(update: Update, context):
+    log_messages(update)
+    chat_id = update.message.chat_id
+    await update.message.reply_text(f"Ваш chat ID: {chat_id}")
 
 async def send_message(chat_id: int, message: str):
     await app.bot.send_message(chat_id=chat_id, text=message)
@@ -34,3 +47,5 @@ async def initialize_app():
     await app.initialize()
 
 app.add_handler(CommandHandler("start", start))
+app.add_handler(CommandHandler("chatid", get_chat_id))
+app.add_handler(MessageHandler(filters.ALL, handle_message))
